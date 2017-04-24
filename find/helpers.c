6c65ba25a940803e59dae164dbe200cbfb90f31b
Vishal Rastogi <vishal.rastogi13@yahoo.com>
2017-04-24 16:20:18 +0000

    commit-1

---
+++ find/helpers.c
@@ -0,0 +1,55 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+ 
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    
+    int low = 0,high = n+1;
+    int mid = (low + high) / 2;
+    while(mid != low)
+    {
+   
+       
+       
+        
+    }
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement an O(n^2) sorting algorithm
+    //selection sort
+    int sml = 0;
+    int count = 0;
+    while(count < n + 1)
+    {
+    for(int i = count; i <= n; i++)
+    {
+       
+    }
+    
+       //swap( &values[count], &values[sml]);
+       
+    
+    }
+    
+    
+    
+    return;
+}
+ 
